fdec440ce0909ba825d2f88430bfcef423164b9f
Antonio Rebordao <antonio@rebordao.net>
2016-12-07 20:14:52 +0000

    Add sync_to_s3().

--- R/do_tarfile.R
+++ R/do_tarfile.R
@@ -2,13 +2,13 @@
 #'
 #' This function compresses a set of files/folders into a tar file.
 #'
-#' @param paths is a vector containing the absolute path of the files/folders to compress.
+#' @param folders is a vector containing the absolute paths of the files/folders to compress.
 #' @param filename is the name of the resulting file sufixed with year, month and day.
 #' @export
 #'
-do_tarfile <- function(paths, filename) {
+do_tarfile <- function(folders, filename) {
 
   fname <- sprintf('%s_%s.tar.gz', filename, format(Sys.Date(), '%Y%m%d'))
 
-  system(sprintf("tar -zcvf %s %s", fname, paste(paths, collapse = ' ')))
+  system(sprintf("tar -zcvf %s %s", fname, paste(folders, collapse = ' ')))
 }

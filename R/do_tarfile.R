6f3ddf05e5a7e204fd5766469a2a1d6b1b6ff81b
Antonio Rebordao <antonio@rebordao.net>
2016-12-20 11:54:31 +0000

    do_tarfile() now accepts relative paths

--- R/do_tarfile.R
+++ R/do_tarfile.R
@@ -1,14 +1,22 @@
-#' Compresses a set of files/folders into a tar file
+#' Compresses a set of files/folders into a tar.gz file
 #'
-#' This function compresses a set of files/folders into a tar file.
+#' Compresses a set of files/folders into a tar.gz file.
+#' Saves output into working directory.
 #'
-#' @param folders is a vector containing the absolute paths of the files/folders to compress.
-#' @param filename is the name of the resulting file sufixed with year, month and day.
+#' @param folders Is a vector containing the paths of the
+#' files/folders to compress. They can be absolute or relative.
+#' @param filename Is the name for the resulting tar.gz file. This name will be
+#' sufixed with year, month and day.
 #' @export
 #'
 do_tarfile <- function(folders, filename) {
-
-  fname <- sprintf('%s_%s.tar.gz', filename, format(Sys.Date(), '%Y%m%d'))
-
-  system(sprintf("tar -zcvf %s %s", fname, paste(folders, collapse = ' ')))
+  
+  fname <- sprintf('%s_%s', filename, format(Sys.Date(), '%Y%m%d'))
+  
+  # Doesn't preserve folder structure, only copies basename of dirs in folders.
+  system(
+    sprintf("tar -cvf %s.tar.gz %s", fname, 
+            paste('-C', 
+                  dirname(normalizePath(folders)), 
+                  basename(normalizePath(folders)), collapse = ' ')))
 }
